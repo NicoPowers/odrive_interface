6d4d7d2561061ffb46184e95524f5cf1863df2fd
Nicolas Montoya <nicolasmontoya@yahoo.com>
2021-08-30 13:24:45 -0700

    trying smalled watchdog timeout

--- scripts/odrive_controller.py
+++ scripts/odrive_controller.py
@@ -19,7 +19,7 @@ my_drive = None
 communication_started = False                 
 watchdog_timer_expired = False   
 watchdog_alive = True
-watchdog_timeout = 5
+watchdog_timeout = 0.25
 last_time = Time()
 
 def watchdog():
